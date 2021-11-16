c419f62bd0914f233252dab76aed5c16e171630d
Dogus Yasayan <dogusyasayans@gmail.com>
2021-11-16 17:08:11 +0300

    Add project files.

---
+++ AdoNetDemo/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿
+namespace AdoNetDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgwProducts = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.name = new System.Windows.Forms.Label();
+            this.unitPrice = new System.Windows.Forms.Label();
+            this.stockAmount = new System.Windows.Forms.Label();
+            this.tbxName = new System.Windows.Forms.TextBox();
+            this.tbxUnitPrice = new System.Windows.Forms.TextBox();
+            this.tbxStockAmount = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwProducts)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgwProducts
+            // 
+            this.dgwProducts.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgwProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwProducts.Location = new System.Drawing.Point(12, 12);
+            this.dgwProducts.Name = "dgwProducts";
+            this.dgwProducts.Size = new System.Drawing.Size(719, 220);
+            this.dgwProducts.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnAdd);
+            this.groupBox1.Controls.Add(this.tbxStockAmount);
+            this.groupBox1.Controls.Add(this.tbxUnitPrice);
+            this.groupBox1.Controls.Add(this.tbxName);
+            this.groupBox1.Controls.Add(this.stockAmount);
+            this.groupBox1.Controls.Add(this.unitPrice);
+            this.groupBox1.Controls.Add(this.name);
+            this.groupBox1.Location = new System.Drawing.Point(12, 238);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(321, 198);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Add a Product";
+            // 
+            // name
+            // 
+            this.name.AutoSize = true;
+            this.name.Location = new System.Drawing.Point(29, 50);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(35, 13);
+            this.name.TabIndex = 0;
+            this.name.Text = "Name";
+            // 
+            // unitPrice
+            // 
+            this.unitPrice.AutoSize = true;
+            this.unitPrice.Location = new System.Drawing.Point(29, 92);
+            this.unitPrice.Name = "unitPrice";
+            this.unitPrice.Size = new System.Drawing.Size(53, 13);
+            this.unitPrice.TabIndex = 0;
+            this.unitPrice.Text = "Unit Price";
+            // 
+            // stockAmount
+            // 
+            this.stockAmount.AutoSize = true;
+            this.stockAmount.Location = new System.Drawing.Point(29, 136);
+            this.stockAmount.Name = "stockAmount";
+            this.stockAmount.Size = new System.Drawing.Size(74, 13);
+            this.stockAmount.TabIndex = 0;
+            this.stockAmount.Text = "Stock Amount";
+            // 
+            // tbxName
+            // 
+            this.tbxName.Location = new System.Drawing.Point(152, 43);
+            this.tbxName.Name = "tbxName";
+            this.tbxName.Size = new System.Drawing.Size(100, 20);
+            this.tbxName.TabIndex = 1;
+            // 
+            // tbxUnitPrice
+            // 
+            this.tbxUnitPrice.Location = new System.Drawing.Point(152, 85);
+            this.tbxUnitPrice.Name = "tbxUnitPrice";
+            this.tbxUnitPrice.Size = new System.Drawing.Size(100, 20);
+            this.tbxUnitPrice.TabIndex = 1;
+            // 
+            // tbxStockAmount
+            // 
+            this.tbxStockAmount.Location = new System.Drawing.Point(152, 129);
+            this.tbxStockAmount.Name = "tbxStockAmount";
+            this.tbxStockAmount.Size = new System.Drawing.Size(100, 20);
+            this.tbxStockAmount.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(152, 155);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(100, 37);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(743, 448);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dgwProducts);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgwProducts)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgwProducts;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox tbxStockAmount;
+        private System.Windows.Forms.TextBox tbxUnitPrice;
+        private System.Windows.Forms.TextBox tbxName;
+        private System.Windows.Forms.Label stockAmount;
+        private System.Windows.Forms.Label unitPrice;
+        private System.Windows.Forms.Label name;
+    }
+}
+
---
+++ AdoNetDemo/ProductDal.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetDemo
+{
+    public class ProductDal
+    {
+        SqlConnection _connection = new SqlConnection(@"server=(localdb)\mssqllocaldb;initial catalog=ETrade; integrated security=true");
+        #region DataTable
+        public DataTable GetAll2()
+        {
+            //Sql database'imiz ile bağlantı kuruyoruz.
+            //we connect with our sql database.
+
+            ConnectionControl();
+            //Sql Komutu oluşturduk.
+            SqlCommand command = new SqlCommand("Select * from Products", _connection);
+            //Sql reader ile komutu okuttuk.
+            SqlDataReader reader = command.ExecuteReader();
+
+            DataTable dataTable = new DataTable();
+            dataTable.Load(reader);
+            reader.Close();
+            _connection.Close();
+            return dataTable;
+        }
+
+        private void ConnectionControl()
+        {
+            if (_connection.State == ConnectionState.Closed)
+            {
+                _connection.Open();
+            }
+        }
+        #endregion
+
+        #region ListVersion
+        public List<Product> GetAll()
+        {
+            //Sql database'imiz ile bağlantı kuruyoruz.
+            //we connect with our sql database.
+
+            ConnectionControl();
+            //Sql Komutu oluşturduk.
+            SqlCommand command = new SqlCommand("Select * from Products", _connection);
+            //Sql reader ile komutu okuttuk.
+            SqlDataReader reader = command.ExecuteReader();
+
+            List<Product> products = new List<Product>();
+
+            while (reader.Read())
+            {
+                Product product = new Product
+                {
+                    Id = Convert.ToInt32(reader["Id"]),
+                    Name = reader["Name"].ToString(),
+                    StockAmount = Convert.ToInt32(reader["StockAmount"]),
+                    UnitPrice = Convert.ToDecimal(reader["UnitPrice"]),
+                };
+                products.Add(product);
+            }
+            reader.Close();
+            _connection.Close();
+            return products;
+        }
+        #endregion
+
+        public void Add(Product product)
+        {
+            ConnectionControl();
+            SqlCommand command = new SqlCommand("Insert into Products (Id,Name,UnitePrice,StockAmount) Values('" + product.Id + "','" + product.Name + "-" + product.UnitPrice + "-" + product.StockAmount + "')", _connection);
+
+            command.Parameters.AddWithValue("@name", product.Name);
+            command.Parameters.AddWithValue("@unitPrice", product.UnitPrice);
+            command.Parameters.AddWithValue("@stockAmount", product.StockAmount);
+            command.ExecuteNonQuery();
+
+            _connection.Close();
+        }
+    }
+
+
+}
